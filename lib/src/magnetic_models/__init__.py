398c7bb34794bd3124f72904bc0e86b4a5544c37
Laszlo Fritsch <laszlofritsch@Laszlos-MacBook-Air-2.local>
2026-03-09 15:27:15 +0100

    Update version to 0.1.1

--- lib/src/magnetic_models/__init__.py
+++ lib/src/magnetic_models/__init__.py
@@ -7,6 +7,8 @@ from .magnetic_models import (
     build_lattice_pairs_periodic,
     calculate_chemical_potential,
     generate_custom_matrices,
+    plot_interactive_mu_c,
+    plot_interactive_mu_dist,
 )
 
 __version__ = "0.1.1"
